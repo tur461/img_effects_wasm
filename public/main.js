1d222e55de404db7dc8fdf11a7b29a565031355d
tur461 <tur461.er@gmail.com>
2022-10-10 00:29:46 +0530

    convert back to base64 dataurl, return to js and render the image

--- public/main.js
+++ public/main.js
@@ -9,19 +9,23 @@
 
   console.log('wasm import success:', rustEnd)
 
+  const newImgTag = document.getElementById('new-img');
   const ip = document.getElementById('upload');
   const fReader = new FileReader();
 
   fReader.onloadend = _ => {
     // read base64 content and also remove the metadata that 
     // is prefixed to any base64 image data
-    const b64Content = fReader.result.replace(
+    let b64Content = fReader.result.replace(
       /^data:image\/(png|jpeg|jpg);base64,/,
       ''
     );
     console.log('image reading completed', b64Content.slice(0, 20));
     // lets call the wasm function
-    rustEnd.process_img(b64Content);
+    // this functions returns image base64 data url 
+    // with grayscale effect applied
+    b64Content = rustEnd.process_img(b64Content);
+    newImgTag.setAttribute('src', b64Content);
   }
 
   ip.addEventListener('change', e => {
--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,34 @@
-use base64::decode as b64dec;
-use image::load_from_memory;
+use base64::{encode as b64enc, decode as b64dec};
 // log_1 functions logs only 1 value
 use web_sys::console::{log_1 as clog1, log_2 as clog2};
 use wasm_bindgen::prelude::*;
-
-fn grayscale() {
-
-}
+use image::{ load_from_memory, ImageOutputFormat };
 
 #[wasm_bindgen]
-pub fn process_img(b64_content: &str) {
+pub fn process_img(b64_content: &str) -> String {
     clog1(&"base64 data recvd".into());
-    let res = b64dec(b64_content);
-    let mut bytes: Vec<u8> = Vec::new();
-    if res.is_ok() {
-        bytes = res.unwrap();
-        clog1(&"base64 data decoded.".into());
-    } else {
-        clog1(&"base64 decoding failure!".into());
-        return;
-    }
+    // lets decode base64 string slice into bytes
+    let mut bytes = b64dec(b64_content).unwrap();
+    clog1(&"base64 data decoded.".into());
 
-    let res = load_from_memory(&bytes[..]);
-    
-    if res.is_ok() {
-        clog1(&"bytes to image done.".into());
-    } else {
-        clog1(&"failed to convert bytes to images!".into());
-        return;
-    }
+    // retruns DynamicImage type
+    let mut img = load_from_memory(&bytes[..]).unwrap();
+    clog1(&"bytes to image done.".into());
 
-    let mut img = res.unwrap();
+    // lets apply grayscale filter
+    // returns same type here as well
     img = img.grayscale();
 
     clog1(&"image converted to grayscale.".into());
+    
+    // lets write image to a buffer as png image
+    let mut buf = vec![];
+    img.write_to(&mut buf, ImageOutputFormat::Png).unwrap();
+    clog1(&"created a png image.".into());
+
+    // encode bytes to base64 back
+    let b64_content = b64enc(&buf);
+    // return the proper formated base64 string
+    // with proper prefix so js takes it as image data
+    format!("data:image/png;base64,{}", b64_content)
 }
