212475c51cabb6e1426e369eaa0ec3d7e98b7832
tur461 <tur461.er@gmail.com>
2022-10-09 23:34:38 +0530

    bytes to image using image crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use base64::decode as b64dec;
+use image::load_from_memory;
 // log_1 functions logs only 1 value
 use web_sys::console::{log_1 as clog1, log_2 as clog2};
 use wasm_bindgen::prelude::*;
@@ -17,5 +18,14 @@ pub fn process_img(b64_content: &str) {
         clog1(&"base64 data decoded.".into());
     } else {
         clog1(&"base64 decoding failure!".into());
+        return;
+    }
+
+    let res = load_from_memory(&bytes[..]);
+    
+    if res.is_ok() {
+        clog1(&"bytes to image done.".into());
+    } else {
+        clog1(&"failed to convert bytes to images!".into());
     }
 }
